c5b5cec736ef0bc76239c838b55fef6de8a2b5f7
Sophie Riddlestone <sophie.riddlestone@gmail.com>
2023-10-08 12:30:20 +0100

    Added basic Chaos Dwarfs #5

---
+++ src/Monsters/Races/ChaosDwarfs.js
@@ -0,0 +1,10 @@
+import ChaosDwarfs from "./ChaosDwarfs/ChaosDwarfs";
+
+const monsters = []
+    .concat(ChaosDwarfs);
+
+export default {
+    key: "ChaosDwarfs",
+    name: "Chaos Dwarfs",
+    monsters,
+};
