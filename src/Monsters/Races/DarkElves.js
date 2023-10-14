8fecee62ab4753a17769b05c6e46e5747bed7951
Andrew Riddlestone <andrew.riddlestone@gmail.com>
2023-10-14 21:41:45 +0100

    Added all dark elves
    #6

--- src/Monsters/Races/DarkElves.js
+++ src/Monsters/Races/DarkElves.js
@@ -1,11 +1,16 @@
 import DarkElfWarriors from "./DarkElves/DarkElves"
-import NaggarothBlackGuards from "./DarkElves/NaggarothBlackGuards";
 import DarkElfBeastmasters from "./DarkElves/DarkElfBeastmasters";
+import DarkElfSorcerers from "./DarkElves/DarkElfSorcerers";
+import NaggarothBlackGuards from "./DarkElves/NaggarothBlackGuards";
+import WitchElves from "./DarkElves/WitchElves";
 
 const monsters = []
     .concat(DarkElfWarriors)
     .concat(DarkElfBeastmasters)
-    .concat(NaggarothBlackGuards);
+    .concat(DarkElfSorcerers)
+    .concat(NaggarothBlackGuards)
+    .concat(WitchElves)
+;
 
 export default {
     key: "DarkElves",
