d5d6cd4e893b3cf7207980fef5afec49e442b9d0
Andrew Riddlestone <andrew.riddlestone@gmail.com>
2023-10-14 14:07:13 +0100

    Created level 2 table
    * Made tables into functions
    * Added dice-roll functions
    * Level 2 incomplete (waiting for some monsters to be added, and Level 3)
    * Level 1 calls Level 2 when appropriate (closes #21)
    * Pass dungeon level and table level to monster functions

--- src/Monsters/Tables/Level2.js
+++ src/Monsters/Tables/Level2.js
@@ -1,35 +1,40 @@
 import {D3, D6} from "../../Utilities/Dice";
-import Level2 from "./Level2";
 
 import Chaos from "../Races/Chaos";
 import {Beastman} from "../Races/Chaos/Beastmen";
+import {Bloodletter} from "../Races/Chaos/Bloodletters";
+import {ChaosHound} from "../Races/Chaos/ChaosHounds";
+import {ChaosWarrior} from "../Races/Chaos/ChaosWarriors";
+import {FiendOfSlaanesh} from "../Races/Chaos/FiendsOfSlaanesh";
+import {BlueHorrorOfTzeentch, PinkHorrorOfTzeentch} from "../Races/Chaos/HorrorsOfTzeentch";
+import {Plaguebearer} from "../Races/Chaos/Plaguebearers";
+
+import ChaosDwarfs from "../Races/ChaosDwarfs";
+import {ChaosDwarf, ChaosDwarfBlunderbuss} from "../Races/ChaosDwarfs/ChaosDwarfs";
 
 import DarkElves from "../Races/DarkElves";
-import {DarkElfWarrior} from "../Races/DarkElves/DarkElves";
 import {NaggarothBlackGuard} from "../Races/DarkElves/NaggarothBlackGuards";
 
 import Monsters from "../Races/Monsters";
-import {CaveSquig} from "../Races/Monsters/CaveSquigs";
 import {Centaur} from "../Races/Monsters/Centaurs";
 import {GiantBat} from "../Races/Monsters/GiantBat";
-import {GiantRat} from "../Races/Monsters/GiantRats";
-import {GiantSpider} from "../Races/Monsters/GiantSpiders";
+import {GiantRat} from "../Races/Monsters/GiantRat";
+import {GiantSpider} from "../Races/Monsters/GiantSpider";
 import {Minotaur} from "../Races/Monsters/Minotaurs";
-import {Ogre} from "../Races/Monsters/Ogres";
-
-import Skaven from "../Races/Skaven";
-import {Clanrat} from "../Races/Skaven/Clanrat";
-import {Stormvermin} from "../Races/Skaven/Stormvermin";
+import {Ogre} from "../Races/Monsters/Ogre";
 
 import OrcsAndGoblins from "../Races/OrcsAndGoblins";
-import {Goblin, GoblinNetter} from "../Races/OrcsAndGoblins/Goblins";
-import {GoblinShaman} from "../Races/OrcsAndGoblins/GoblinShamans";
-import {GoblinSquigHunter} from "../Races/OrcsAndGoblins/GoblinSquigHunters";
+import {BlackOrc} from "../Races/OrcsAndGoblins/BlackOrcs";
+import {Goblin, GoblinBoss} from "../Races/OrcsAndGoblins/Goblins";
 import {Hobgoblin} from "../Races/OrcsAndGoblins/Hobgoblins";
-import {Orc} from "../Races/OrcsAndGoblins/Orcs";
 import {SavageOrcShaman} from "../Races/OrcsAndGoblins/OrcShamans";
 import {SavageOrc} from "../Races/OrcsAndGoblins/SavageOrcs";
 import {Snotling} from "../Races/OrcsAndGoblins/Snotlings";
+
+import Skaven from "../Races/Skaven";
+import {GutterRunner} from "../Races/Skaven/SkavenAssassins";
+import {PlagueCenserBearer} from "../Races/Skaven/PlagueCenserBearers";
+import {SkavenPoisonGlobadier} from "../Races/Skaven/SkavenPoisonGlobadiers";
 
 import Undead from "../Races/Undead";
 import {Ghoul} from "../Races/Undead/Ghouls";
@@ -39,317 +44,11 @@ import {Zombie} from "../Races/Undead/Zombies";
 export const monsters = [
     Object.assign((dungeonLevel, tableLevel) => {
         return [{
-            qty: D3(),
-            monster: Ogre(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Monsters,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 2,
-            monster: Beastman(dungeonLevel, tableLevel),
+            qty: D6() + 1,
+            monster: ChaosWarrior(dungeonLevel, tableLevel),
         }];
     }, {
         race: Chaos,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 3,
-            monster: Stormvermin(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Skaven,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 6,
-            monster: Snotling(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: OrcsAndGoblins,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 6,
-            monster: GiantBat(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Monsters,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6(2),
-            monster: GiantSpider(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Monsters,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6(2),
-            monster: GiantRat(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Monsters,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6(2),
-            monster: Orc(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: OrcsAndGoblins,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6(2),
-            monster: Clanrat(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Skaven,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [
-            {
-                qty: D6(),
-                monster: SavageOrc(dungeonLevel, tableLevel),
-            },
-            {
-                qty: 1,
-                monster: SavageOrcShaman(dungeonLevel, tableLevel),
-            },
-        ];
-    }, {
-        race: OrcsAndGoblins,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6(),
-            monster: GoblinNetter(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: OrcsAndGoblins,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 2,
-            monster: Skeleton(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Undead,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6(),
-            monster: Zombie(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Undead,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6(2),
-            monster: Ghoul(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Undead,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 3,
-            monster: DarkElfWarrior(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: DarkElves,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6(2),
-            monster: GiantSpider(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Monsters,
-    }),
-
-    // 1D3 Wild Cave Squigs or (1D3 Trained Cave Squigs and Goblin Squig Hunters)
-    Object.assign((dungeonLevel, tableLevel) => {
-        if (D6() <= 3) {
-            return [{
-                qty: D3(),
-                monster: CaveSquig(dungeonLevel, tableLevel),
-            }];
-        } else {
-            const qty = D3();
-            return [
-                {
-                    qty,
-                    monster: CaveSquig(dungeonLevel, tableLevel),
-                },
-                {
-                    qty: 2 * qty,
-                    monster: GoblinSquigHunter(dungeonLevel, tableLevel),
-                },
-            ];
-        }
-    }, {
-        race: OrcsAndGoblins,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 6,
-            monster: Snotling(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: OrcsAndGoblins,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 6,
-            monster: GiantBat(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Monsters,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 3,
-            monster: Hobgoblin(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: OrcsAndGoblins,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [
-            {
-                qty: D6() + 6,
-                monster: Goblin(dungeonLevel, tableLevel),
-            },
-            {
-                qty: 1,
-                monster: GoblinShaman(dungeonLevel, tableLevel),
-            },
-        ];
-    }, {
-        race: OrcsAndGoblins,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D3(),
-            monster: Minotaur(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Monsters,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6(2),
-            monster: Orc(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: OrcsAndGoblins,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 6,
-            monster: Snotling(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: OrcsAndGoblins,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 6,
-            monster: GiantBat(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Monsters,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6(2),
-            monster: GiantSpider(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Monsters,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6(2),
-            monster: GiantRat(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Monsters,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6(2),
-            monster: Clanrat(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Skaven,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6(2),
-            monster: Skeleton(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: Undead,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 6,
-            monster: Goblin(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: OrcsAndGoblins,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 3,
-            monster: Hobgoblin(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: OrcsAndGoblins,
-    }),
-
-    Object.assign((dungeonLevel, tableLevel) => {
-        return [{
-            qty: D6() + 2,
-            monster: NaggarothBlackGuard(dungeonLevel, tableLevel),
-        }];
-    }, {
-        race: DarkElves,
     }),
 
     Object.assign((dungeonLevel, tableLevel) => {
@@ -363,17 +62,316 @@ export const monsters = [
 
     Object.assign((dungeonLevel, tableLevel) => {
         return [{
-            qty: D3(),
+            qty: 3,
+            monster: Ogre(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Monsters,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: 3,
             monster: Minotaur(dungeonLevel, tableLevel),
         }];
     }, {
         race: Monsters,
     }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: 12,
+            monster: Snotling(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: OrcsAndGoblins,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: 12,
+            monster: GiantBat(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Monsters,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: 12,
+            monster: GiantSpider(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Monsters,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: 12,
+            monster: GiantRat(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Monsters,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: 12,
+            monster: Hobgoblin(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: OrcsAndGoblins,
+    }),
+
+    // TODO: 1D3 Rat Ogres
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [
+            {
+                qty: D6(),
+                monster: PinkHorrorOfTzeentch(dungeonLevel, tableLevel),
+            },
+            {
+                qty: 0,
+                monster: BlueHorrorOfTzeentch(dungeonLevel, tableLevel),
+            },
+        ];
+    }, {
+        race: Chaos,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: D6(),
+            monster: FiendOfSlaanesh(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Chaos,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: D6() + 2,
+            monster: Bloodletter(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Chaos,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: D6() + 2,
+            monster: Plaguebearer(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Chaos,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: D6(),
+            monster: ChaosHound(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Chaos,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: D6() + 4,
+            monster: BlackOrc(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: OrcsAndGoblins,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: D6(2),
+            monster: SavageOrc(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: OrcsAndGoblins,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: D6() + 2,
+            monster: Beastman(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Chaos,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [
+            {
+                qty: D6(),
+                monster: GutterRunner(dungeonLevel, tableLevel),
+            },
+            {
+                qty: D6(),
+                monster: PlagueCenserBearer(dungeonLevel, tableLevel),
+            },
+            {
+                qty: D6(),
+                monster: SkavenPoisonGlobadier(dungeonLevel, tableLevel),
+            },
+        ];
+    }, {
+        race: Skaven,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [
+            {
+                qty: 12,
+                monster: Goblin(dungeonLevel, tableLevel),
+            },
+            {
+                qty: 1,
+                monster: GoblinBoss(dungeonLevel, tableLevel),
+            },
+        ];
+    }, {
+        race: OrcsAndGoblins,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: D6(2),
+            monster: NaggarothBlackGuard(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: DarkElves,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: D6() + 1,
+            monster: ChaosWarrior(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Chaos,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: D3(),
+            monster: Centaur(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Monsters,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: 3,
+            monster: Ogre(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Monsters,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: 3,
+            monster: Minotaur(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Monsters,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: 12,
+            monster: Skeleton(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Undead,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: 6,
+            monster: Zombie(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Undead,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: 12,
+            monster: Ghoul(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: Undead,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [
+            {
+                qty: D6(2),
+                monster: SavageOrc(dungeonLevel, tableLevel),
+            },
+            {
+                qty: 1,
+                monster: SavageOrcShaman(dungeonLevel, tableLevel),
+            },
+        ];
+    }, {
+        race: OrcsAndGoblins,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: D6() + 2,
+            monster: ChaosDwarf(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: ChaosDwarfs,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: D6() + 2,
+            monster: ChaosDwarfBlunderbuss(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: ChaosDwarfs,
+    }),
+
+    // TODO: 3 Bull Centaurs
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [{
+            qty: 12,
+            monster: Hobgoblin(dungeonLevel, tableLevel),
+        }];
+    }, {
+        race: OrcsAndGoblins,
+    }),
+
+    Object.assign((dungeonLevel, tableLevel) => {
+        return [
+            {
+                qty: D3(),
+                monster: Minotaur(dungeonLevel, tableLevel),
+            },
+            {
+                qty: D3(),
+                monster: Ogre(dungeonLevel, tableLevel),
+            },
+        ];
+    }, {
+        race: Monsters,
+    }),
 ];
 
-export default function (dungeonLevel = 1, race = null) {
+export default function (dungeonLevel = 2, race = null) {
     if (Math.random() < (1.0 / 18.0)) {
-        return Level2(dungeonLevel, race);
+        // TODO: return Level3(dungeonLevel, race);
     }
     const table = race
         ? monsters.filter(row => row.race === race)
@@ -381,5 +379,5 @@ export default function (dungeonLevel = 1, race = null) {
     if(table.length === 0) {
         return [];
     }
-    return table[Math.floor(Math.random() * table.length)](dungeonLevel, 1);
+    return table[Math.floor(Math.random() * table.length)](dungeonLevel, 2);
 }
