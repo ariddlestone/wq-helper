3cfbc642a054cfe0d65dd05c3038e51fa4b188a3
Andrew Riddlestone <andrew.riddlestone@gmail.com>
2024-07-20 14:22:05 +0100

    #17: Added level 5 table

--- src/Monsters/Tables.js
+++ src/Monsters/Tables.js
@@ -2,12 +2,14 @@ import Level1 from "./Tables/Level1";
 import Level2 from "./Tables/Level2";
 import Level3 from "./Tables/Level3";
 import Level4 from "./Tables/Level4";
+import Level5 from "./Tables/Level5";
 
 const tables = {
     1: Level1,
     2: Level2,
     3: Level3,
     4: Level4,
+    5: Level5,
 };
 
 export default function(level, race = null) {
